fc62574aaf33f484ff153aa0c6fa9d452a1fd1f5
huanglei <code.huanglei@gmail.com>
2019-08-07 16:23:27 +0800

    modify

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
-// import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
 import App from '@/App'
-import Header from '@/components/header'
-
-Vue.use(Header)
-
-// Vue.use(ElementUI)
-
-// Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
